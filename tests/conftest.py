c95a98058249c330e9f1fbca2555e949cdfabff5
zdmiller12 <zdmiller12@gmail.com>
2025-03-11 22:22:36 -0400

    add testdata files

--- tests/conftest.py
+++ tests/conftest.py
@@ -14,3 +14,8 @@ from osm_scene import DEFAULT_DIR_IO
 @pytest.fixture(scope="session")
 def dir_io():
     return Path(__file__).parent.parent / DEFAULT_DIR_IO
+
+
+@pytest.fixture(scope="session")
+def dir_testdata():
+    return Path(__file__).parent / "testdata"
